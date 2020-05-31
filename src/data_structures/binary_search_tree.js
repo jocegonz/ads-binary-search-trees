f1e39991f95b90511cd8fdf20fa6a2e0f1778298
Jocelyn <jocegonz@gmail.com>
2020-05-31 10:56:34 -0700

    WIP: delete mostly complete;

--- src/data_structures/binary_search_tree.js
+++ src/data_structures/binary_search_tree.js
@@ -17,6 +17,44 @@ class BinarySearchTree {
 
   insert(key, value = true) {
     // TODO
+    let node = new this.Node({key, value});
+    if (this._count === 0) {
+      node.parent = undefined;
+      this._root = node;
+      this._count += 1;
+      return node;
+    } else {
+      let currentNode  = this._root;
+
+      while (currentNode) {
+        if (key < currentNode.key) {
+          if (currentNode.left) {
+            currentNode  = currentNode.left;
+          } else {
+            node.parent = currentNode;
+            currentNode.left = node;
+            this._count += 1;
+
+            return currentNode;
+          }
+          
+        } else if (key > currentNode.key) {
+          if (currentNode.right) {
+            currentNode = currentNode.right;
+          } else {
+            node.parent = currentNode;
+            currentNode.right = node;
+            this._count += 1;
+
+            return currentNode;
+          }
+        } else {
+          currentNode.value  = value;
+          return currentNode;
+        } 
+      } 
+    }
+
   }
 
   lookup(key) {
@@ -35,6 +73,113 @@ class BinarySearchTree {
 
   delete(key) {
     // TODO (tests first!)
+    let node = this.lookup(key);
+    if (node) {
+      this._count -= 1;
+        let findAndDelete = (currentNode, nodeToDelete) => {
+          if (nodeToDelete < currentNode.key) {
+            if (currentNode.left) {
+              findAndDelete(currentNode.left, nodeToDelete);
+            } 
+          } else if (nodeToDelete > currentNode.key) {
+            if (currentNode.right) {
+              findAndDelete(currentNode.right, nodeToDelete);
+            }
+          } else {            
+            if (!currentNode.left && !currentNode.right) {
+              if (currentNode.parent) {
+                if (currentNode.parent.right && currentNode.parent.right.key === nodeToDelete) {
+                    currentNode.parent.right = undefined;
+                  } else {
+                  this._root = undefined;
+                }
+              } else {
+                currentNode = undefined;
+              }
+            } else if (!currentNode.left) {
+              if (currentNode.parent) {
+                if (currentNode.parent.right.key === nodeToDelete) {
+                  currentNode.parent.right = currentNode.right;
+                } else {
+                  currentNode.parent.left = currentNode.right;
+                  currentNode.right.parent = currentNode.parent;
+                }
+              }
+            } else if (!currentNode.right) {
+              if (currentNode.parent) {
+                if (currentNode.parent.right.key === nodeToDelete) {
+                  currentNode.parent.right = currentNode.left;
+                } else {
+                  currentNode.parent.left = currentNode.left;
+                }
+              }
+            } else {
+              let connectingNode = currentNode.right;
+
+              while (connectingNode.left) {
+                connectingNode = connectingNode.left
+              }
+              findAndDelete(currentNode.right, connectingNode.key);
+              currentNode.key = connectingNode.key;
+              currentNode.value = connectingNode.value;
+              if (currentNode.left) {
+                currentNode.left.parent = currentNode;
+              }
+              if (currentNode.right) {
+                currentNode.right.parent = currentNode;
+              }
+            }
+          }
+        }
+        findAndDelete(this._root, key);
+      // }
+        // let currentNode  = this._root;
+
+    //     while (currentNode) {
+    //       if (key < currentNode.key) {
+    //         if (currentNode.left) {
+    //           currentNode  = currentNode.left;
+    //         } 
+    //         // else {
+    //         //   node.parent = currentNode;
+    //         //   currentNode.left = node;
+    //         //   this._count += 1;
+  
+    //         //   // return currentNode;
+    //         // }
+            
+    //       } else if (key > currentNode.key) {
+    //         if (currentNode.right) {
+    //           currentNode = currentNode.right;
+    //         } 
+    //         // else {
+    //         //   node.parent = currentNode;
+    //         //   currentNode.right = node;
+    //         //   this._count += 1;
+  
+    //           // return currentNode;
+    //         // }
+    //       } else {
+    //         let newParent;
+    //         if (currentNode.left) {
+    //           newParent = currentNode.left;
+    //         } else if (currentNode.right) {
+    //           newParent = currentNode.right
+    //         } 
+    //         else {
+    //           console.log(`@@@ leaf nodes: ${JSON.stringify(currentNode.left)} :::parent ${currentNode.parent}`)
+    //         //   let previousParent = currentNode.parent;
+    //         //   previousParent.left = undefined;
+    //         }
+    //         break;
+    //         // currentNode.value  = value;
+    //           // return currentNode;
+    //       }
+    //     }   
+    //   }
+    }
+    return node ? key : undefined;
+
   }
 
   count() {
